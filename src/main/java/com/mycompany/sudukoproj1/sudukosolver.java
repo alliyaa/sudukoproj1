d37634c573fea7a61214714f1375000120c5404b
alliya <alliy@LAPTOP-HFA4BKS6>
2023-10-15 19:08:34 +0300

    Suduko Solver Project

--- src/main/java/com/mycompany/sudukoproj1/sudukosolver.java
+++ src/main/java/com/mycompany/sudukoproj1/sudukosolver.java
@@ -9,6 +9,7 @@ public class sudukosolver {
   public static void main(String[] args) {
   Scanner input = new Scanner(System.in); 
   int[][] board = new int [9][9]; //initialize the 2d array board 
+   System.out.println("Enter your board");
    for(int i=0; i<9; i++)
    { 
        for(int j=0; j<9; j++)
